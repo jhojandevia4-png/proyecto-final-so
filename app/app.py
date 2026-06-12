92039b7b9a18a41765f58a7535e91b3e2d6b98b4
Jhojan Devia <jhojandevia4@gmail.com>
2026-06-12 16:56:22 -0500

    Actualizar sitio web final y enlace del video

--- app/app.py
+++ app/app.py
@@ -1,1 +1,9 @@
-print("Servicio Python ejecutandose correctamente")
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Backend Python funcionando")
+
+HTTPServer(("0.0.0.0", 5000), Handler).serve_forever()
